a22ea7158fa81b940db04548a22d3b86ff2d31be
Damian Isaacs <Damianisaacs@live.com>
2020-09-09 16:22:43 -0700

    Need Config
    ???

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    token: 'NjQyMjE1NDE1MDg2NTc5NzIx.XcomNw.-6wp-H74MFuPmhx3NQHlCzfG6MY',
+    owner: '239307675840544768',
+    prefix: '!',
+    mongoURI: 'mongodb+srv://TheArchive:NQ1y7bAupTvVBUtM@cluster0.7hzdh.mongodb.net/Archive',
+    guildid: '239310955639603200',
+    defaultSettings: {
+        prefix: '!',
+        welcomeChannel: 'welcome',
+        welcomeMsg: 'Welcome {{user}} to {{guild}}!',
+        modRole: 'Moderator',
+        adminRole: 'Administrator'
+    },
+    googlsheet: "1GK_9Z3e1kXHZJiHfOcBAlXd1PUrqaY9OWNr6azi1JN4",
+    googlekeys: {
+      "type": "service_account",
+      "project_id": "mimir-1573191734374",
+      "private_key_id": "f2847196de32c5a8e022a1d0cb5a72ca49e6e392",
+      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCj2mSznF9QFDCA\narTqxCySZTfhqnNGVIhBYb4p+A4PHs448XMHxyQYPvWhteEufqazgBxsaGSOgETm\nMHpt36rfgtlwKXEqAQnxdiMYuGJwvxp2CncbQIbvHhRsLU1VH0j8QhDjAFevA5IK\nvblenRSR22DEgfxj/SFvOZIpl+yOV3x0iIk0VQxb6E6Dzo8J/rMXwa62N7wNe1Kb\nKCWy/uBEtW2GJ6SmemH5xzrCcHjZyewSlPY8S6eqIFzgc1KhJHfCKq9m2NM8hXtz\n2IC2oczZ67A1V3U8iTFhac6uhcEYyGbys9gnz9FAyOQdJSYwcTN51lHYhp8jwzbR\ncbf6rvaDAgMBAAECgf8e4l8hijtVBEfhuet6E48/nBC5SazM1zz47VHJxFOAK3Ub\nNexJ7JAx/tspe1FBjXmN8tI97JdI5HE9LDrsfQwIUha/Gica3jW3+ePx90v6J+4/\n4Tuk7kDsH6qp1v3hMuqsQfG4r0j52LCg72m4cSjcjF5lOfD9yKNFPOFGL8Y0fPFh\n15PkpwMDfrB74ucWFNH5o1Zq9hS89Sx9DkxPmJjocXn0fFUfFvXLVA/2421sWjdm\niDPzkpOmD+s1hch2DQi4tzafuH4QSP3Pc26bRkdaMGBGryFp9aV5CzHtfvQbaEEU\nsGfD/8Ls4kuc38o35vSAhT+0HrHL4pibfh9FQokCgYEA3ysDFWIfCz4+y+lUrooA\nD7X/0Hty6eERuXXrIGIZOBxgVdkwf5HV5v1vjCaUw/zizuvUSPNWXUxXV9Or7b/u\nIJ9XKxrwYWh7PZ00s3gMDaUZilPU1506fpb0ajl2PUOn35E8e35RNbQ0Fq8z7kwU\naoORVC+t1OXIlHwBX1opQxsCgYEAu/V0L6m0ML3iH2FapYsDZStewnelFeWdabSh\nkEyNL5gfUqBTAN7eE0x7Cj6AqYnLZT/wREAxPh21IZ/IUKdy/HJ2rlPc107E0pZC\njd6f5jAjS+Ew4PWOOP/sX/ULFH+ZFcsPyVh7upJ/dmHp5ZMUdBjJK/410f9rDTGF\ne/336LkCgYEAqoC7N2AjJbzlz3cyqrNmvponm4plzoiVuSN9g91PNU6Az7ZDVoP+\ncB/bVLG5YU/ZnYwSQbYgmuGTrddb+SZ1cdkriRRo6ZMg7X8pX6DtXJNyQvwvlpha\ns5LzpCDI1J/XJ3lYQ2XL+AL49Qfg3kSkKIjixPY0kn8I02E2zzocYlcCgYBdxjs+\nYGOnAcVkNJUeDHoYKogva7PwdcuQZm9I5/h0q2fNOHHQ+GBBrqGgXcihTT8t97Vu\nx7wJqpao/Ofa3YAv1QfDN9Hxlbn6+r6fu1d5Z8DkZaiWQ8y+QWj0AvSROIvu5I2u\nvbeVQOvY60bfo/7LYWZUzZKaf90wCun3D6F0+QKBgQCnYA9WoZFc3lPE4GhCV7zo\n7CL1WXihHy+uxGH/qdcRCvrTsRVKv4dickpdTekmVQM+VbBvAvLSM+7L4Q4DYYcu\nTcSaQ42AraeHyohAHiDf9KeZckcVxWlDRJIGKNa+nLtRgBklKNV2wYOKE+LmFMOv\nAcvSPAKXHds7LMj73Hr+IA==\n-----END PRIVATE KEY-----\n",
+      "client_email": "mimir-130@mimir-1573191734374.iam.gserviceaccount.com",
+      "client_id": "109136988437673245570",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mimir-130%40mimir-1573191734374.iam.gserviceaccount.com"
+    }
+};
